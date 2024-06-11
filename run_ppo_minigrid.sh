8b4d6e14df798acf62bf25b48d4104ccb234c255
Antonio Pio Ricciardi <antonioricciardi93@gmail.com>
2024-06-11 15:24:25 +0200

    updated

---
+++ run_ppo_minigrid.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+#
+# bash run_ppo_carracing.sh --run-mode ppo --env-id CarRacing-v2 --background green --car-mode standard --anchors-alpha 0
+# bash run_ppo_carracing.sh --run-mode ppo-rel --env-id CarRacing-v2 --background multicolor --car-mode standard --anchors-alpha 0.999
+# bash run_ppo_carracing.sh --run-mode ppo_resnet --env-id CarRacing-v2 --car-mode standard --background green
+#!/bin/bash
+
+# usage function
+usage() {
+    echo "Usage: $0 [--run-mode RUN_MODE] [--env-id ENV_ID] [--background BACKGROUND] [--car-mode CAR_MODE]"
+    echo "Options:"
+    echo "  --run-mode     Set the run mode (default: ppo)"
+    echo "  --env-id       Set the environment ID (default: CarRacing-v2)"
+    echo "  --grid-size    Set the grid size (default: 6)"
+    echo "  --goal-shape   Set the goal shape (default: circle)"
+    echo "  --goal-color   Set the goal color (default: red)"
+    echo "  --item-color   Set the item color (default: green)"
+    echo "  --wall-color   Set the wall color (default: grey)"
+    exit 0
+}
+
+
+grid_size, goal_shape, goal_shape, goal_color, item_color, wall_color,
+
+# default values
+run_mode="ppo"
+env_id="CarRacing-v2"
+grid_size=6
+goal_shape="circle"
+goal_color="red"
+item_color="green"
+wall_color="grey"
+anchors_alpha=0
+
+# parse command line arguments
+# if argument is usage, print usage and exit
+if [ "$1" = "--usage" ]; then
+    usage
+fi
+
+# Parse named arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --run-mode)
+            run_mode="$2"
+            shift 2
+            ;;
+        --env-id)
+            env_id="$2"
+            shift 2
+            ;;
+        --grid-size)
+            grid_size="$2"
+            shift 2
+            ;;
+        --goal-shape)
+            goal_shape="$2"
+            shift 2
+            ;;
+        --goal-shape)
+            goal_shape="$2"
+            shift 2
+            ;;
+        --goal-color)
+            goal_color="$2"
+            shift 2
+            ;;
+        --item-color)
+            item_color="$2"
+            shift 2
+            ;;
+        --wall-color)
+            wall_color="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid argument: $2"
+            exit 1
+            ;;
+    esac
+done
+
+
+# print the values
+echo "Run Mode: $run_mode"
+echo "Environment ID: $env_id"
+echo "Grid Size: $grid_size"
+echo "Goal Shape: $goal_shape"
+echo "Goal Color: $goal_color"
+echo "Item Color: $item_color"
+echo "Wall Color: $wall_color"
+echo "Anchors Alpha: $anchors_alpha"
+# if [ "$1" == "ppo" ]
+if [ $run_mode == "ppo" ]
+then
+    python ppo_minigrid.py --track --wandb-project-name rlrepr_ppo_minigrid --exp-name "$car_mode"_"$background"_rgb --env-id minigrid-custom --seed 1 --num-envs 16 --grid-size $grid_size --goal-shape $goal_shape --goal-color $goal_color --item-color $item_color --wall-color $wall_color --stack-n 4 --total-timesteps 500000
+
+    python ppo_minigrid.py --track --wandb-project-name rlrepr_ppo_minigrid --exp-name "$car_mode"_"$background"_rgb --env-id minigrid-custom --seed 2 --num-envs 16 --grid-size $grid_size --goal-shape $goal_shape --goal-color $goal_color --item-color $item_color --wall-color $wall_color --stack-n 4 --total-timesteps 500000
+
+    python ppo_minigrid.py --track --wandb-project-name rlrepr_ppo_minigrid --exp-name "$car_mode"_"$background"_rgb --env-id minigrid-custom --seed 3 --num-envs 16 --grid-size $grid_size --goal-shape $goal_shape --goal-color $goal_color --item-color $item_color --wall-color $wall_color --stack-n 4 --total-timesteps 500000
+
+    python ppo_minigrid.py --track --wandb-project-name rlrepr_ppo_minigrid --exp-name "$car_mode"_"$background"_rgb --env-id minigrid-custom --seed 4 --num-envs 16 --grid-size $grid_size --goal-shape $goal_shape --goal-color $goal_color --item-color $item_color --wall-color $wall_color --stack-n 4 --total-timesteps 500000
+# elif [ $run_mode == "ppo-rel" ]
+# then
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "rel_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 1 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-relative --anchors-path data/anchors/"$env_id"/rgb_ppo_transitions_"$background"_obs.pkl --anchors-indices-path data/anchor_indices/"$env_id"_3136_anchor_indices_from_4000.txt --total-timesteps 5000000 --anchors-alpha $anchors_alpha
+    
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "rel_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 2 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-relative --anchors-path data/anchors/"$env_id"/rgb_ppo_transitions_"$background"_obs.pkl --anchors-indices-path data/anchor_indices/"$env_id"_3136_anchor_indices_from_4000.txt --total-timesteps 5000000 --anchors-alpha $anchors_alpha
+
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "rel_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 3 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-relative --anchors-path data/anchors/"$env_id"/rgb_ppo_transitions_"$background"_obs.pkl --anchors-indices-path data/anchor_indices/"$env_id"_3136_anchor_indices_from_4000.txt --total-timesteps 5000000 --anchors-alpha $anchors_alpha
+
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "rel_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 4 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-relative --anchors-path data/anchors/"$env_id"/rgb_ppo_transitions_"$background"_obs.pkl --anchors-indices-path data/anchor_indices/"$env_id"_3136_anchor_indices_from_4000.txt --total-timesteps 5000000 --anchors-alpha $anchors_alpha
+# elif [ $run_mode == "ppo_resnet" ]
+# then
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "resnet_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 1 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-resnet
+
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "resnet_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 2 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-resnet
+
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "resnet_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 3 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-resnet
+
+#     python ppo_carracing_discrete_rgb_relrepr_end_to_end.py --track --wandb-project-name rlrepr_ppo_carracing_discrete --exp-name "resnet_$car_mode"_"$background"_rgb --env-id CarRacing-custom --seed 4 --num-envs 16 --background $background --car-mode $car_mode --stack-n 4 --total-timesteps 5000000 --use-resnet
+else
+    echo "Invalid argument"
+fi
