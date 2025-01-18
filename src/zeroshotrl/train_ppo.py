d3618760cc2bb5816a7f924749ff5b46f5b4be39
Antonio Pio Ricciardi <antonioricciardi93@gmail.com>
2025-01-18 11:05:05 +0100

    saving anchors (hopefully)

--- src/zeroshotrl/train_ppo.py
+++ src/zeroshotrl/train_ppo.py
@@ -453,12 +453,12 @@ class PPOTrainer_vec:
             eval_best_score = eval_avg_reward
             # best_score = eval_avg_reward
             if not self.pretrained:
-                best_model_params["encoder"] = self.agent.encoder.state_dict()
                 if self.use_relative:
                     # save anchors buffer and then upload the model to wandb.
                     # this is to ensure that the model is saved with the updated anchors just once
                     # to avoid saving heavy models with anchors at each evaluation
                     self.agent.encoder.save_anchors_buffer()
+                best_model_params["encoder"] = self.agent.encoder.state_dict()
             best_model_params["policy"] = self.agent.policy.state_dict()
         print(
             "Current eval_avg_reward: ",
